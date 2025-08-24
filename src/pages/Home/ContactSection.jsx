437421148287e16eab555cec8ca25fde2361a9d9
Ayubxon Obidov <128991489+ayubxon0880@users.noreply.github.com>
2025-08-25 00:40:00 +0500

    link changed

--- src/pages/Home/ContactSection.jsx
+++ src/pages/Home/ContactSection.jsx
@@ -2,9 +2,9 @@ export default function ContactSection() {
     return (
         <section className="bg-yellow-400 py-12">
             <div className="flex justify-center items-center">
-                <button className="bg-blue-500 text-white px-10 py-4 rounded-xl text-xl font-bold shadow-md hover:bg-blue-600 transition duration-300">
+                <a href={"https://n1221770.alteg.io/company/1167547/record-type?o="} className="bg-blue-500 text-white px-10 py-4 rounded-xl text-xl font-bold shadow-md hover:bg-blue-600 transition duration-300">
                     ЗАПИСАТЬСЯ
-                </button>
+                </a>
             </div>
         </section>
     )
